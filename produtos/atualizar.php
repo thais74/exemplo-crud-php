7eaadb6dcbb421712f9db42facb4cd1ecc48dcfd
Thais <thaissoares567123@gmail.com>
2023-08-31 08:41:43 -0300

    Update atualizar.php

--- produtos/atualizar.php
+++ produtos/atualizar.php
@@ -7,6 +7,24 @@ $fabricantes = lerFabricantes($conexao);
 $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
 $produtos = lerUmProduto($conexao, $id);
 
+if (isset($_POST['atualizar'])){
+    $nome = filter_input(INPUT_POST, "nome", FILTER_SANITIZE_SPECIAL_CHARS);
+    
+    $preco = filter_input(INPUT_POST, "preco", FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+
+    $quantidade = filter_input(INPUT_POST, "quantidade", FILTER_SANITIZE_NUMBER_INT);
+
+    $fabricanteId = filter_input(INPUT_POST, "fabricante", FILTER_SANITIZE_NUMBER_INT);
+
+    $descricao = filter_input(INPUT_POST, "descricao", FILTER_SANITIZE_SPECIAL_CHARS);
+
+    atualizarProduto(
+        $conexao, $id, $nome, $quantidade, $descricao, $fabricanteId
+    );
+
+    header("location:visualizar.php");
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
